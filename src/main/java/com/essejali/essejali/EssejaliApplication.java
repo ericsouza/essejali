f04bdcf387d18dfee15ff3bdc01fa462981bf77e
Eric Souza <ericsouza0801@gmail.com>
2020-06-24 22:02:47 -0300

    starting project

---
+++ src/main/java/com/essejali/essejali/EssejaliApplication.java
@@ -0,0 +1,13 @@
+package com.essejali.essejali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssejaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssejaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/essejali/essejali/EssejaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essejali.essejali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssejaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
